a98d100a976f167e35ae751906174d930e21aa39
mrwhoareyou <2359146115@qq.com>
2016-09-09 17:20:51 +0800

    slide date-picker

--- Application/Runtime/Cache/Home/61e1e5ff8ded6860e4dfdd8d372c9014.php
+++ Application/Runtime/Cache/Home/61e1e5ff8ded6860e4dfdd8d372c9014.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="renderer" content="webkit|ie-comp|ie-stand">
-	<meta name="keywords" content="社团详情" />
+	<meta name="keywords" content="社团创建" />
 	<meta name="description" content=""/>
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 	<meta http-equiv="Cache-Control" content="no-transform" /> 
@@ -25,13 +25,13 @@
 		<link rel="stylesheet" type="text/css" href="/collegial/Public/css/IE/ie678.css" />
 	<![endif]-->
 	
-	<title>社团详情</title>
+	<title>社团创建</title>
 	<!-- <link rel="icon" href="favicon.ico" mce_href="favicon.ico" type="image/x-icon">
 	<link rel="bookmark" href="favicon.ico" mce_href="favicon.ico" type="image/x-icon" /> 
 	<link rel="shortcut icon" href="favicon.ico" mce_href="favicon.ico" type="image/x-icon"> -->
 	<link rel="stylesheet" href="/collegial/Public/css/core/core.css">
 	<link rel="stylesheet" href="/collegial/Public/css/style.css">
-	<script type="text/javascript" src="/collegial/Public/plugins/jquery-1.11.2.js"></script>
+	<script type="text/javascript" src="/collegial/Public/plugins/zepto.min.js"></script>
 </head>
 <body>
 	<style>body {background-color: #eee}</style>
@@ -44,7 +44,7 @@
 			<div class="ccreate-detail">
 				<ul>
 					<li><span> 社团名称</span><input type="text" placeholder="请输入社团名称"></li>
-					<li class="pr"><span>社团级别</span><input type="text" value="校级组织" readonly><img src="/collegial/Public/images/arrow_right.png" alt=""></li>
+					<li class="show-dpicker pr"><span>社团级别</span><input type="text" value="校级组织" readonly><img src="/collegial/Public/images/arrow_right.png" alt=""></li>
 				</ul>
 			</div>
 			<p class="ccreate-title">
@@ -54,19 +54,79 @@
 				<ul>
 					<li><span>申请人姓名</span><input type="text" placeholder="请输入您的姓名"></li>
 					<li><span>身份证号码</span><input type="text" placeholder="请输入您的身份证号码"></li>
-					<li class="scode-send-wrap"><span>手机号码</span><input type="text" placeholder="手机号码"><input type="button" class="" value="发送验证码"></li>
+					<li class="scode-send-wrap"><span>手机号码</span><input type="text" placeholder="手机号码"><input type="button" value="发送验证码"></li>
 					<li><span>验证码</span><input type="text" placeholder="请输入您收到的验证码"></li>
 				</ul>
 			</div>
 			<button class="submit">立即创建</button>
 		</form>
 	</div>
+	<div class="date-picker">
+		<p><span class="dpicker-finish">完成</span></p>
+		<div class="dpicker-body">
+			<ul>
+				<li class="dpicker-item empty sub-active"></li>
+				<li class="dpicker-item active">校级组织</li>
+				<li class="dpicker-item sub-active">院级组织</li>
+				<li class="dpicker-item">兴趣组织</li>
+				<li class="dpicker-item">校级组织</li>
+				<li class="dpicker-item">院级组织</li>
+				<li class="dpicker-item">兴趣组织</li>
+				<li class="dpicker-item empty"></li>
+			</ul>
+		</div>
+	</div>
 	<script>
+
 		$(function() {
+			$(".dpicker-body li")
 			$(".submit").click(function() {
 				alert("12");
 			});
+
+			var rate = 0;
+			$(".dpicker-body").scroll(function() {
+				rate = setPickerStyle(rate)
+			});
+			
+			$(".dpicker-finish").click(function() {
+				var text = $(".dpicker-item.active").text();
+				alert(text);
+				$(".date-picker").css({'bottom':'-17.4rem'})
+			});
+
+			$(".show-dpicker").click(function() {
+				console.log($(".date-picker"));
+				$(".date-picker").css({'bottom':0});
+			});
+
+			$(".scode-send-wrap input[type = 'button']").click(function() {
+				$(this).addClass('active');
+			});
+
+			setPickerStyle(0);
 		})
+		function setPickerStyle(rate) {
+			var i, len,
+				scrollTop = $(".dpicker-body").scrollTop(),
+				newRate = parseInt(scrollTop / 38);
+
+			if(newRate === rate) {
+				return rate;
+			}
+
+			var itemList = $(".dpicker-item");
+
+			if($(itemList[newRate + 1]).hasClass('empty')) {
+				return newRate;
+			}
+
+			itemList = $(".dpicker-item").removeClass('active').removeClass('sub-active');
+
+			$(itemList[newRate + 1]).addClass('active').prev().addClass('sub-active').next().next().addClass('sub-active');
+
+			return newRate;
+		}
 	</script>
 
 </body>
