b56a5cf5507b8daf25207a614612c8d720e5648e
mrwhoareyou <2359146115@qq.com>
2016-11-07 16:32:22 +0800

    me topic

--- Public/script/topic.js
+++ Public/script/topic.js
@@ -1,39 +1,53 @@
 $(function() {
-	$("#topic-list").on('click', '.fa-heart', function() {
+	$("#topic-list").on('click', '.fa-heart', function(e) {
+		// alert();
 		var that = $(this);
-		// that.removeClass('fa-heart').addClass('fa-heart-o').parent().removeClass('active');
-		$.get('', function(data) {
-			if (data.status == 1) {
-				that.removeClass('fa-heart').addClass('fa-heart-o').parent().removeClass('active');
-			}
-		})
+		setTimeout(function() {
+			that.removeClass('fa-heart').addClass('fa-heart-o').parent().removeClass('active');
+		}, 10)
+
+		// $.get('', function(data) {
+		// 	if (data.status == 1) {
+		// 		that.removeClass('fa-heart').addClass('fa-heart-o').parent().removeClass('active');
+		// 	}
+		// })
 	});
 
 	$("#topic-list").on('click', '.fa-heart-o', function() {
 		var that = $(this);
-		$.get('', function(data) {
-			if (data.status == 1) {
-				that.addClass('fa-heart').removeClass('fa-heart-o').parent().addClass('active');
-			}
-		})
+		setTimeout(function() {
+			that.addClass('fa-heart').removeClass('fa-heart-o').parent().addClass('active');
+		}, 10)
+
+		// $.get('', function(data) {
+		// 	if (data.status == 1) {
+		// 		that.addClass('fa-heart').removeClass('fa-heart-o').parent().addClass('active');
+		// 	}
+		// })
 	});
 
 	$("#topic-list").on('click', '.fa-star-o', function() {
 		var that = $(this);
-		$.get('', function(data) {
-			if (data.status == 1) {
+		setTimeout(function() {
 				that.removeClass('fa-star-o').addClass('fa-star').parent().addClass('active');
-			}
-		})
+			}, 10)
+			// $.get('', function(data) {
+			// 	if (data.status == 1) {
+			// 		that.removeClass('fa-star-o').addClass('fa-star').parent().addClass('active');
+			// 	}
+			// })
 	});
 
 	$("#topic-list").on('click', '.fa-star', function() {
 		var that = $(this);
-		$.get('', function(data) {
-			if (data.status == 1) {
-				that.removeClass('fa-star').addClass('fa-heart-o').parent().removeClass('active');
-			}
-		})
+		setTimeout(function() {
+				that.removeClass('fa-star').addClass('fa-star-o').parent().removeClass('active');
+			}, 10)
+			// $.get('', function(data) {
+			// 	if (data.status == 1) {
+			// 		that.removeClass('fa-star').addClass('fa-heart-o').parent().removeClass('active');
+			// 	}
+			// })
 	});
 
 	$("#topic-list").on('click', '.topic-ibottom-item:first-child', function() {
